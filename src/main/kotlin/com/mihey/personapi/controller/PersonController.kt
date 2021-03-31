b8f272d9751b77683d8395542ea4bf490623a49d
Mikhail Dolgov <otetc82@yandex.ru>
2021-03-31 20:32:51 +0300

    add methods to controller

--- src/main/kotlin/com/mihey/personapi/controller/PersonController.kt
+++ src/main/kotlin/com/mihey/personapi/controller/PersonController.kt
@@ -4,9 +4,7 @@ import com.mihey.personapi.model.Person
 import com.mihey.personapi.service.PersonService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("persons")
@@ -14,7 +12,37 @@ class PersonController(val personService: PersonService) {
 
     @GetMapping
     fun getPersons(): ResponseEntity<List<Person>> {
-        return ResponseEntity(personService.getPersons(),HttpStatus.OK)
+        return ResponseEntity(personService.getPersons(), HttpStatus.OK)
     }
 
+    @GetMapping("/{id}")
+    fun getPersonbyId(@PathVariable id: Int): ResponseEntity<Person> {
+        return ResponseEntity(personService.findById(id), HttpStatus.OK)
+    }
+
+    @GetMapping("/{name}")
+    fun getPersonByname(@PathVariable name: String): ResponseEntity<List<Person>> {
+        return ResponseEntity(personService.findByName(name), HttpStatus.OK)
+    }
+
+    @GetMapping("/{lastName}")
+    fun getPersonbyLastname(@PathVariable lastName: String): ResponseEntity<List<Person>> {
+        return ResponseEntity(personService.findByLastName(lastName), HttpStatus.OK)
+    }
+
+    @PostMapping
+    fun savePerson(@RequestBody person: Person): ResponseEntity<Person> {
+        return ResponseEntity(personService.savePerson(person), HttpStatus.OK)
+    }
+
+    @PutMapping
+    fun updatePerson(@RequestBody person: Person): ResponseEntity<Person> {
+        return ResponseEntity(personService.updatePerson(person), HttpStatus.OK)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePersonById(@PathVariable id: Int): ResponseEntity<String> {
+        personService.deleteById(id)
+        return ResponseEntity("User successfully deleted", HttpStatus.OK)
+    }
 }
--- src/main/kotlin/com/mihey/personapi/dao/impl/PersonDaoImplementation.kt
+++ src/main/kotlin/com/mihey/personapi/dao/impl/PersonDaoImplementation.kt
@@ -8,34 +8,42 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Component
 
 @Component
-class PersonDaoImplementation(@Autowired val jdbcTemplate: JdbcTemplate): PersonDao {
+class PersonDaoImplementation(@Autowired val jdbcTemplate: JdbcTemplate) : PersonDao {
 
     override fun getPersons(): List<Person> {
-        val sql="SELECT * FROM persons"
+        val sql = "SELECT * FROM persons"
         return jdbcTemplate.query(sql, PersonMapper())
     }
 
     override fun findById(id: Int): Person {
-        TODO("Not yet implemented")
+        val sql = "SELECT * FROM persons WHERE id=?"
+        return jdbcTemplate.queryForObject(sql, PersonMapper(), id) ?: throw RuntimeException("No such person!")
     }
 
     override fun findByName(name: String): List<Person> {
-        TODO("Not yet implemented")
+        val sql = "SELECT * FROM persons WHERE name=?"
+        return jdbcTemplate.query(sql, PersonMapper(), name)
     }
 
     override fun findByLastName(lastName: String): List<Person> {
-        TODO("Not yet implemented")
+        val sql = "SELECT * FROM persons WHERE last_name=?"
+        return jdbcTemplate.query(sql, PersonMapper(), lastName)
     }
 
     override fun savePerson(person: Person): Person {
-        TODO("Not yet implemented")
+        val sql = "INSERT INTO persons (name,last_name) VALUES (?, ?)"
+        jdbcTemplate.update(sql,person.name,person.lastName)
+        return person
     }
 
     override fun updatePerson(person: Person): Person {
-        TODO("Not yet implemented")
+        val sql = "UPDATE persons SET name=?, last_name=?  WHERE id=?"
+        jdbcTemplate.update(sql,person.name,person.lastName,person.id)
+        return person
     }
 
     override fun deleteById(id: Int) {
-        TODO("Not yet implemented")
+        val sql = "DELETE FROM persons WHERE id=?"
+        jdbcTemplate.update(sql, id)
     }
 }
